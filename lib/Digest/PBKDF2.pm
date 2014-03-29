2c3a31eca527f406de889f581c0695b0d4aa024a
Amiri Barksdale <amiri@arisdottle.net>
2014-03-29 19:29:36 +0000

    Import of AMIRI/Digest-PBKDF2-0.010 from CPAN.
    gitpan-cpan-distribution: Digest-PBKDF2
    gitpan-cpan-version:      0.010
    gitpan-cpan-path:         AMIRI/Digest-PBKDF2-0.010.tar.gz
    gitpan-cpan-author:       AMIRI
    gitpan-cpan-maturity:     released

--- lib/Digest/PBKDF2.pm
+++ lib/Digest/PBKDF2.pm
@@ -6,7 +6,7 @@ use parent "Digest::base";
 use Crypt::PBKDF2 0.112020;
 
 BEGIN {
-	our $VERSION = '0.009'; # VERSION
+	our $VERSION = '0.010'; # VERSION
 }
 
 #ABSTRACT: This module is a subclass of Digest using the Crypt::PBKDF2 algorithm.
@@ -54,7 +54,7 @@ sub digest {
         if @{ $self->{_entries} } > 1;
     my $data = join( '', @string );
 
-    my $crypt = Crypt::PBKDF2->new( encoding => $self->{encoding}, salt_len => length($salt||'') );
+    my $crypt = Crypt::PBKDF2->new( encoding => ($self->{encoding}||'ldap'), salt_len => length($salt||'') );
     my $return = $crypt->generate( $data, $salt );
     $self->reset;
     $return;
@@ -94,7 +94,7 @@ That's about it.
 
 =item new
 
-Create a new Digest::PBKDF2 object. This defaults to using the "crypt" encoding
+Create a new Digest::PBKDF2 object. This defaults to using the "ldap" encoding
 available in Crypt::PBKDF2--please see L<Crypt::PBKDF2> for details.
 
 =item clone
@@ -130,7 +130,7 @@ Amiri Barksdale, E<lt>abarksdale@campusexplorer.comE<gt>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2011 by Campus Explorer, Inc.
+Copyright (c) 2014 by Campus Explorer, Inc.
 
 L<http://www.campusexplorer.com>
 
